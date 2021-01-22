ca5b6d40ec112802a53838d2519d745b4591b182
anthonycc2 <anthonycc2@users.noreply.github.com>
2021-01-22 19:39:40 -0300

    Correção de erros

--- src/main/java/br/unit/lojaUnit/controlador/ControladorItemVenda.java
+++ src/main/java/br/unit/lojaUnit/controlador/ControladorItemVenda.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.unit.lojaUnit.entidade.ItemVenda;
@@ -34,14 +33,9 @@ public class ControladorItemVenda {
 
 	@PostMapping(path = "/add")
 	public @ResponseBody String inserir(
-			@RequestParam(required = true, name = "venda") int idVenda,
-			@RequestParam(required = true, name = "produto") int idProduto,
-			@RequestParam(required = true, name = "quantidade") int quantidade,
-			@RequestParam(required = true, name = "valor") double valorUnitario) {
-		if (repositorioVenda.existsById(idVenda) && repositorioProduto.existsById(idProduto)) {
-			Venda venda = new Venda(idVenda);
-			Produto produto = new Produto(idProduto);
-			ItemVenda itemVenda = new ItemVenda(venda, produto, quantidade, valorUnitario);
+			@RequestBody ItemVenda itemVenda) {
+		if (repositorioVenda.existsById(itemVenda.getId().getVenda().getId())
+				&& repositorioProduto.existsById(itemVenda.getId().getProduto().getId())) {
 			repositorio.save(itemVenda);
 			return "Saved";
 		} else {
